cd74e65ef36221046438d7ff4e178a47333e5677
naveen-ranium <naveen@ranium.in>
2023-08-29 18:36:04 +0530

    removed code which is not in use

--- app/Http/Controllers/NeoController.php
+++ app/Http/Controllers/NeoController.php
@@ -6,7 +6,6 @@ use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Request;
 use Exception;
-use Illuminate\Support\Facades\Validator;
 use Carbon\Carbon;
 class NeoController extends Controller
 {
@@ -55,32 +54,6 @@ class NeoController extends Controller
                 return redirect()->back()
                 ->with('error_message', $neoApiData['error_message']);
             }
-
-            //if key exists in array means we got data successfully from API
-            // if (array_key_exists("element_count",$neoApiData) && array_key_exists("near_earth_objects",$neoApiData))
-            // {
-            //     $asteroidsCount = $neoApiData['element_count'];
-            //     $getNeoStatstics = $this->getNeoStatstics($neoApiData);
-
-            //     $neoDatesdata = array_keys($getNeoStatstics);
-            //     $neoAstroidData = array_values($getNeoStatstics);
-
-            //     //for getting FastestAsteroidData
-            //     $getFastestAsteroid = $this->getFastestAsteroid($neoApiData);
-
-            //     //for getting ClosestAsteroidData
-            //     $getClosestAsteroid = $this->getClosestAsteroid($neoApiData);
-
-            //     //for getting Average Size
-            //     $getAverageSizeOfAsteroid = $this->getAverageSizeOfAsteroid($neoApiData,$asteroidsCount);
-
-            //     return view('barchart', compact('asteroidsCount','getFastestAsteroid','getClosestAsteroid','getAverageSizeOfAsteroid','neoDatesdata', 'neoAstroidData'));
-            // }
-            // else
-            // {
-            //     return redirect()->back()
-            //     ->with('error_message', $neoApiData['error_message']);
-            // }
         }
         catch (Exception $e)
         {
@@ -92,29 +65,29 @@ class NeoController extends Controller
     private function getChartData(Collection $asteroids)
     {
         return $asteroids->mapWithKeys(fn ($dayAsteroids, $date) => [$date => count($dayAsteroids)])
-            ->sortKeys()
-            ->toArray();
+                ->sortKeys()
+                ->toArray();
     }
 
     private function getFastestAsteroidData(Collection $asteroids)
     {
         return $asteroids->flatMap(fn ($values) => $values)
-            ->sortByDesc('close_approach_data.0.relative_velocity.kilometers_per_hour')
-            ->first();
+                ->sortByDesc('close_approach_data.0.relative_velocity.kilometers_per_hour')
+                ->first();
     }
 
     private function getClosestAsteroidData(Collection $asteroids)
     {
         return $asteroids->flatMap(fn ($values) => $values)
-            ->sortBy('close_approach_data.0.miss_distance.kilometers')
-            ->first();
+                ->sortBy('close_approach_data.0.miss_distance.kilometers')
+                ->first();
     }
 
     private function getAverageSizeOfAsteroidData(Collection $asteroids,$asteroidsCount)
     {
 
         $totalSize = $asteroids->flatMap(fn ($values) => $values)
-            ->sum('estimated_diameter.kilometers.estimated_diameter_max');
+                    ->sum('estimated_diameter.kilometers.estimated_diameter_max');
 
         return $averageSize = $asteroidsCount > 0 ? ($totalSize / $asteroidsCount) : 0;
     }
@@ -130,90 +103,5 @@ class NeoController extends Controller
             'end_date' => $endDate->format('Y-m-d'),
             'api_key' => $apiKey,
         ])->json());
-    }
-
-    private function getNeoStatstics($neoApiData)
-    {
-
-        $neoDataByItems = [];
-        foreach ($neoApiData['near_earth_objects'] as $key => $value) {
-            $neoDataByItems[$key] = count($value);
-        }
-
-        //Sorting Data by date order
-        ksort($neoDataByItems);
-
-        return $neoDataByItems;
-    }
-
-    private function getFastestAsteroid($data)
-    {
-        //creating array to return all calculated data
-        $fastestAsteroidData = [];
-
-        // Variable inizialization to find the fastest asteroid from the data
-        $maxSpeed = 0;
-
-        foreach ($data['near_earth_objects'] as $dateAsteroids) {
-
-            foreach ($dateAsteroids as $asteroid) {
-                //calculate Speed
-
-                $speedKph = $asteroid['close_approach_data'][0]['relative_velocity']['kilometers_per_hour'];
-
-                if ($speedKph > $maxSpeed) {
-                    $maxSpeed = $speedKph;
-                    $fastestAsteroidData['fastest_asteroid_id'] = $asteroid['id'];
-                    $fastestAsteroidData['max_speed'] = $maxSpeed;
-                }
-            }
-        }
-
-        return $fastestAsteroidData;
-    }
-
-    private function getClosestAsteroid($data)
-    {
-         //creating array to return all calculated data
-         $closestAsteroidData = [];
-
-         // Variable inizialization to find the closest asteroid from the data
-         $closestAsteroid = null;
-         $closestDistance = PHP_INT_MAX;
-
-         foreach ($data['near_earth_objects'] as $dateAsteroids) {
-
-            foreach ($dateAsteroids as $asteroid) {
-
-                // Calculate Distance
-                $distance = $asteroid['close_approach_data'][0]['miss_distance']['kilometers'];
-
-                if ($distance < $closestDistance) {
-                    $closestAsteroidData['closest_asteroid_id'] = $asteroid['id'];
-                    $closestAsteroidData['closest_distance'] = $distance;
-                }
-            }
-        }
-
-        return $closestAsteroidData;
-    }
-
-    private function getAverageSizeOfAsteroid($data,$asteroidsCount)
-    {
-
-        // Variable inizialization to calculate the average size of asteroids from the data
-        $totalSize = 0;
-
-        foreach ($data['near_earth_objects'] as $dateAsteroids) {
-
-            foreach ($dateAsteroids as $asteroid) {
-
-                //calculate Average Size
-                $totalSize += $asteroid['estimated_diameter']['kilometers']['estimated_diameter_max'];
-                $averageSize = $asteroidsCount > 0 ? ($totalSize / $asteroidsCount) : 0;
-            }
-        }
-
-        return $averageSize;
     }
 }
